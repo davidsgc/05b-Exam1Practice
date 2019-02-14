d57467c065070ddd7171faf80b84f840f5e75382
davidsgc <davidsgc@rose-hulman.edu>
2019-02-13 19:01:16 -0500

    problem 3 started

--- src/problem3.py
+++ src/problem3.py
@@ -31,18 +31,7 @@ import rosegraphics as rg
 
 def main():
     """ Calls the   TEST   functions in this module. """
-
-        point1 = rg.Point(point.x + (20 * k), point.y + (10 * k))
-        point2 = rg.Point(point1.x, point1.y + 50)
-        line = rg.Line(point1, point2)
-        if 2 * k <= 12:
-            line.thickness = 1 + (2 * k)
-        else:
-            line.thickness = 13
-        sum = sum + line.thickness
-        line.attach_to(window)
-    window.render()
-    return sum
+    run_test_problem3a()
 
 def run_test_problem3b():
     """ Tests the   problem3b   function. """
@@ -67,7 +56,7 @@ def run_test_problem3b():
 def run_test_problem3a():
     """ Tests the   problem3a   function. """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement this TEST function.
+    # DONE: 2. Implement this TEST function.
     #   It TESTS the  problem1a  function defined below.
     #   Include at least **   5   ** tests (we wrote four for you).
     # -------------------------------------------------------------------------
@@ -181,7 +170,7 @@ def problem3a(window, point, n):
         :type n:      int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   Note that you should write its TEST function first (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
@@ -191,6 +180,18 @@ def problem3a(window, point, n):
     # -------------------------------------------------------------------------
     sum = 0
     for k in range(n):
+        point1 = rg.Point(point.x + 20 * k, point.y + 10 * k)
+        point2 = rg.Point(point.x + 20 * k, point.y + 10 * k + 50)
+        line = rg.Line(point1, point2)
+        if 2 * k <= 12:
+            line.thickness = line.thickness + (2 * k)
+        else:
+            line.thickness = 13
+        sum = sum + line.thickness
+        line.attach_to(window)
+    window.render()
+    return sum
+
 def problem3b(m, point1):
     """
     See   problem3b_picture.pdf   in this project for pictures
@@ -241,7 +242,13 @@ def problem3b(m, point1):
     #    DIFFICULTY:      8 or 9
     #    TIME ESTIMATE:   20 to 30 minutes.
     # -------------------------------------------------------------------------
-
+    window = rg.RoseWindow(400, 600)
+    sum = 0
+    for k in range(m):
+        sum = sum + problem3a(window, rg.Point(point1.x, point1.y + (60 * k)), 3 + (2 * k))
+    window.render()
+    window.close_on_mouse_click()
+    return sum
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
